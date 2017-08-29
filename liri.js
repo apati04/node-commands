f858cdf70081866c14170b07831b45b4408bc5ce
Andrew Patipaksiri <apax714@gmail.com>
2017-08-29 14:31:03 -0700

    spotify command complete

--- liri.js
+++ liri.js
@@ -9,23 +9,30 @@ const fetchTweets = (usr, q = {screen_name: 'apatipak8'}) => {
     tweets.forEach(({ text }, i) => console.log(`@${q.screen_name} : ${text}`));
   });
 }
-
 const fetchSongs = (usr, query) => {
-  console.log(usr);
-  usr.search({type: 'track', query}).then((err, data)=>console.log(data, query));
+  usr.search({type: 'track', query})
+     .then(({tracks: { items }}) => {
+      const { artists, preview_url, name } = items;
+      items.filter(({preview_url}) => preview_url !== null)
+            .forEach(({ album, artists, preview_url, name })=> {
+              let band = artists[0].name, alb = album.name;
+              let res = JSON.stringify({ band, album: alb, song: name, preview_url},null, 2);
+              console.log(res);
+            })
+    })
 }
 
-function liri([, , cmd, ...input] = process.argv) {
-  switch (cmd) {
-    case 'my-tweets':
-      fetchTweets(new Twitter(twitterKeys));
-      break;
-    case 'spotify-this-song':
-      let str = input.join(' ');
-      fetchSongs(str);
-    default:
-      console.log(cmd, 'defualt');
-  }
+[, , cmd, ...input] = process.argv;
+
+
+switch (cmd) {
+  case 'my-tweets':
+    fetchTweets(new Twitter(twitterKeys));
+    break;
+  case 'spotify-this-song':
+    let str = input.join(' '),
+        user = new Spotify(spotifyKeys);
+    fetchSongs(user, str);
+  default:
+    console.log(cmd, 'defualt');
 }
-// ititialize program
-liri();
