6dc9cef235f098a0690e710d62a30983c91d37dd
Andrew Patipaksiri <apax714@gmail.com>
2017-08-29 13:51:51 -0700

    config

--- keys.js
+++ keys.js
@@ -1,8 +1,15 @@
 console.log('this is loaded');
 
+
+
 exports.twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: 'RTl6g7THF8yIWDVogYRwsupFc',
+  consumer_secret: 'r1Ap7kC3HNWg2HUhvh96eRwTSAMe4KttEDdSr3nUx3o6Z1FwPo',
+  access_token_secret: 'E6Em9EhyPczGhAezct5nEvWPpspiofzFTndnDTdPERRMY',
+  access_token_key: '902049998795227137-EOqxXzy14NmnL8i4kqvBVqjRX5voVZW',
+}
+
+exports.spotifyKeys = {
+  id: "7758ecbfc12c4a54877d2cc0e446c0b6",
+  secret: "4a0c6bdad2aa4d6d8bcc6b10a5d77f1a"
 }
--- liri.js
+++ liri.js
@@ -1,1 +1,31 @@
-console.log('liri');
+const Twitter = require('twitter');
+const request = require('request');
+const Spotify = require('node-spotify-api');
+const { twitterKeys, spotifyKeys } = require('./keys');
+
+
+const fetchTweets = (usr, q = {screen_name: 'apatipak8'}) => {
+  usr.get('statuses/user_timeline', q, (err, tweets) => {
+    tweets.forEach(({ text }, i) => console.log(`@${q.screen_name} : ${text}`));
+  });
+}
+
+const fetchSongs = (usr, query) => {
+  console.log(usr);
+  usr.search({type: 'track', query}).then((err, data)=>console.log(data, query));
+}
+
+function liri([, , cmd, ...input] = process.argv) {
+  switch (cmd) {
+    case 'my-tweets':
+      fetchTweets(new Twitter(twitterKeys));
+      break;
+    case 'spotify-this-song':
+      let str = input.join(' ');
+      fetchSongs(str);
+    default:
+      console.log(cmd, 'defualt');
+  }
+}
+// ititialize program
+liri();
